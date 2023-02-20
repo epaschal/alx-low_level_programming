23e15724cfe8abb9a3720c5b014ee08939f3a817
Paschal <paschalejioguody@gmail.com>
2023-02-20 04:10:03 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,7 +5,7 @@
  */
 int main(void)
 {
-	char str1[] = "\"Programming is like building a multilingual puzzle \n";
+	char str1[] = "\"Programming is like building a multilingual puzzle ";
 
 	puts(str1);
 
