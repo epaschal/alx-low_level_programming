2d2678538786be90e67e941772252c557ddfe2e5
Paschal <paschalejioguody@gmail.com>
2023-02-20 12:47:30 -0800

    Data types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,13 +11,11 @@ int main(void)
 	long int l;
 	long long int x;
 	float f;
-	double d;
 
 	printf("The size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
 	printf("The size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
 	printf("The size of a long int: %lu byte(s)\n", (unsigned long)sizeof(l));
 	printf("The size of long long int: %lu byte(s)\n", (unsigned long)sizeof(x));
 	printf("The size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
-	printf("The size of a double: %lu byte(s)\n", (unsigned long)sizeof(d));
 	return (0);
 }
