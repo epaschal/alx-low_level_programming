b88f6117978881b6b016a156185676d8036a29c5
Paschal <paschalejioguody@gmail.com>
2023-02-20 04:07:18 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 
+/**
+ * main - A function to output a string
+ */
 int main(void)
 {
-	char str1[] = "\"Programming is like building a multilingual puzzle\n";
+	char str1[] = "\"Programming is like building a multilingual puzzle \n";
 
 	puts(str1);
 
