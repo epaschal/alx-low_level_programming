af459b639099a060181e0de2f9437a6fc3118c02
Paschal <paschalejioguody@gmail.com>
2023-02-26 13:34:52 -0800

    Times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * times_table - multiplication
+ */
+void times_table(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if ((i * j) > 9)
+			{
+				_putchar((i * j) / 10 + '0');
+				_putchar((i * j) % 10 + '0');
+			}
+			else
+			{
+				_putchar(i * j + '0');
+			}
+			if (j < 9 && (i * j) > 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			else if (j < 9 && (i * j) <= 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
