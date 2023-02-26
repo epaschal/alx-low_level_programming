dc7bff28b22ea7275a33692dfdae0d1e71d51971
Paschal <paschalejioguody@gmail.com>
2023-02-26 14:37:51 -0800

    Print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,9 +10,21 @@ void print_to_98(int n)
 {
 	int i;
 
-	for (i = n; i < 99; i++)
+	for (i = n; i < 99 ; i++)
 	{
-		printf("%d ,", i);
+		printf("%d", i);
+		if (i < 98)
+		{
+			printf(", ");
+		}
+	}
+	for (i = n; i > 97; i--)
+	{
+		printf("%d", i);
+		if (i > 98)
+		{
+			printf(", ");
+		}
 	}
 	printf("\n");
 }
