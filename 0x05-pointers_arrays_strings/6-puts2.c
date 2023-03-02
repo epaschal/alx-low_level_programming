53086db0532caa430f6bc326fc72cd206aaf3760
Paschal <paschalejioguody@gmail.com>
2023-03-02 11:57:42 -0800

    Puts

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character
+ * @str: string
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+		i++;
+	}
+}
