776e2c8b0d6feb10c760c3460c8e0fe2197a5bc5
Paschal <paschalejioguody@gmail.com>
2023-03-03 08:39:26 -0800

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -15,18 +15,7 @@ int _strcmp(char *s1, char *s2)
 	{
 		if (s1[i] != s2[i])
 		{
-			if (s1[i] < s2[i])
-			{
-				return (s1[i] - s2[i]);
-			}
-			else if (s1[i] > s2[i])
-			{
-				return (s1[i] - s2[i]);
-			}
-		}
-		else
-		{
-			return (0);
+			return (s1[i] - s2[i]);
 		}
 	}
 	return (0);
