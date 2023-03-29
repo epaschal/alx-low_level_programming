0356a3b42517f684d6368f67ec6452a64864c052
Paschal <paschalejioguody@gmail.com>
2023-03-28 20:53:59 -0700

    argc

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - function
+ * @argc: counter
+ * @argv: string
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
